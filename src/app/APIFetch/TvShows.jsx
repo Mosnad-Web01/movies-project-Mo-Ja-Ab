b830355f32a06d0460846e8cd6b902488a2b3e60
51l3n7 <abdullah22shiban42@gmail.com>
2024-09-20 12:06:00 +0300

    feat(Home): Added responsive shadcn navbar component

--- src/app/APIFetch/TvShows.jsx
+++ src/app/APIFetch/TvShows.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Card from "../components/Card/Card";
+import Card from "../../components/component/Card/Card";
 
 const tmdbAPI = process.env.NEXT_PUBLIC_APIKEY;
 
