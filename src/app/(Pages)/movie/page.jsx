75ffd19ba4bff7c451b9ec405858acfa0156b4f2
51l3n7 <abdullah22shiban42@gmail.com>
2024-09-21 12:08:39 +0300

    fix(movies): fixed

--- src/app/(Pages)/movie/page.jsx
+++ src/app/(Pages)/movie/page.jsx
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-"use client"
-import React, { useState } from "react"
-import TrendingMovies from "@/app/APIFetch/weekMovies"
-import PopularMoviesAPI from "@/app/APIFetch/todayMoviesAPI"
-
-const MoviesPage = ({ movie }) => {
-  const [selectedAPI, setSelectedAPI] = useState("popular")
-
-  const handleAPIChange = (apiType) => {
-    setSelectedAPI(apiType)
-  }
-
-  return (
-    <div className="bg-background">
-      <div className="flex items-center justify-center gap-x-3">
-        <h1 className="text-2xl text-center cursor-pointer text-foreground font-Righeous">
-          TRENDING:{" "}
-        </h1>
-        <h1
-          className={`text-2xl text-center cursor-pointer text-foreground hover:text-orange-600 ${
-=======
 "use client";
 import React, { useState } from "react";
 import TrendingMovies from "@/app/APIFetch/weekMovies";
@@ -42,7 +20,6 @@ const MoviesPage = ({ movie }) => {
         </h1>
         <h1
           className={`text-2xl text-center cursor-pointer hover:text-orange-600 ${
->>>>>>> 07de35d39e3b125c6889193002e17b6e9c8d24f6
             selectedAPI === "popular" ? "text-orange-600" : ""
           }`}
           onClick={() => handleAPIChange("popular")}
@@ -50,11 +27,7 @@ const MoviesPage = ({ movie }) => {
           Today
         </h1>
         <h1
-<<<<<<< HEAD
-          className={`text-2xl text-center cursor-pointer text-foreground hover:text-orange-600 ${
-=======
           className={`text-2xl text-center cursor-pointer hover:text-orange-600 ${
->>>>>>> 07de35d39e3b125c6889193002e17b6e9c8d24f6
             selectedAPI === "trending" ? "text-orange-600" : ""
           }`}
           onClick={() => handleAPIChange("trending")}
@@ -69,14 +42,7 @@ const MoviesPage = ({ movie }) => {
         <PopularMoviesAPI PopularMoviesAPI={movie} />
       )}
     </div>
-<<<<<<< HEAD
-  )
-}
-
-export default MoviesPage
-=======
   );
 };
 
 export default MoviesPage;
->>>>>>> 07de35d39e3b125c6889193002e17b6e9c8d24f6
